80672001df7a58a458cefe3a2b8811c38269f971
liuyu685 <liuyu21@jd.com>
2018-08-13 20:45:24 +0800

    my eureka

--- src/main/java/eurekademo/EurekaApplication.java
+++ src/main/java/eurekademo/EurekaApplication.java
@@ -2,7 +2,10 @@ package eurekademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -12,6 +15,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
+
+	@Bean
+	@LoadBalanced  //@LoadBalanced实现负载均衡
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EurekaApplication.class, args);
